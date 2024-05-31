1f9299f0b4e2e5dc560cef5f573931b34b5ae66a
b3b <ash.b3b@gmail.com>
2024-05-31 21:45:28 +0200

    Update the main scene

--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -1,6 +1,11 @@
 import { Scene } from "phaser";
+import { PriceFetcher } from "../PriceFetcher";
 
 let debugText;
+
+let prices;
+let priceData;
+let pointsCounter = 0;
 
 export class MainScene extends Scene
 {
@@ -17,12 +22,33 @@ export class MainScene extends Scene
     create() {
         debugText = this.add.text(10, 10, '', { fontSize: '16px', fill: '#ffffff' });
         debugText.setScrollFactor(0);
+        this.setupEvents()
+
+        prices = new PriceFetcher({emitter: this.emitter});
+        prices.connect();
     }
 
     update() {
         debugText.setText([
-            `Main scene Loaded`,
+            `Price: ${priceData?.price}`,
+            `Points counter: ${pointsCounter}` 
         ]);
+    }
+
+    setupEvents() {
+        let emitter = this.emitter = new Phaser.Events.EventEmitter();
+        emitter.on("PRICE", this.priceHandler.bind(this));
+    }
+
+    priceHandler(data) {
+        if (priceData != undefined && priceData.ts > data.ts) {
+            return;
+        }
+        // if (pointsCounter > 5) {
+        //     return;
+        // }
+        priceData = data;
+        pointsCounter += 1;
     }
 
 }
