e1467757400b8fd9396aa5611d0f9049336244c6
b3b <ash.b3b@gmail.com>
2024-05-31 17:06:00 +0200

    Update Phaser config

--- src/main.js
+++ src/main.js
@@ -6,15 +6,32 @@ import { Preloader } from './scenes/Preloader';
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
+
+const factor = 2;
+var w = 450 * factor;
+var h = 800 * factor;
+
 const config = {
     type: Phaser.AUTO,
     width: 1024,
     height: 768,
     parent: 'game-container',
     backgroundColor: '#028af8',
+    type: Phaser.WEBGL,
+    powerPreference:"high-performance",
+    // transparent: true,
+    physics: {
+        default: "arcade",
+        arcade: {
+            fps: 60,
+            debug: true,
+        }
+    },        
     scale: {
         mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: w,
+        height: h,        
     },
     scene: [
         Boot,
