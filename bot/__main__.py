9a44fafdcc55d8c89b52878a7e4de4375525954b
b3b <ash.b3b@gmail.com>
2024-05-31 17:20:46 +0200

    Deploy initial TON Connect bot
    Based on example from https://github.com/nessshon/aiogram-tonconnect/tree/main/aiogram_tonconnect

---
+++ bot/__main__.py
@@ -0,0 +1,54 @@
+import os
+
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.redis import RedisStorage
+from aiogram_tonconnect.handlers import AiogramTonConnectHandlers
+from aiogram_tonconnect.middleware import AiogramTonConnectMiddleware
+from aiogram_tonconnect.tonconnect.storage.base import ATCRedisStorage
+from aiogram_tonconnect.utils.qrcode import QRUrlProvider
+
+from .handlers import router
+from .throttling import ThrottlingMiddleware
+
+
+BOT_TOKEN = "1234567890:QWERTYUIOPASDFGHJKLZXCVBNM"
+REDIS_DSN = "redis://localhost:6379/0"
+MANIFEST_URL = os.environ.get("MANIFEST_URL")
+EXCLUDE_WALLETS = ["telegram-wallet", "tonhub"]
+
+
+async def main():
+    storage = RedisStorage.from_url(os.environ.get("REDIS_DSN", REDIS_DSN))
+    # Creating a bot object with the token and HTML parsing mode
+    bot = Bot(os.environ.get("BOT_TOKEN", BOT_TOKEN), parse_mode="HTML")
+
+    await bot.delete_webhook()
+
+    # Creating a dispatcher object using the specified storage
+    dp = Dispatcher(storage=storage)
+
+    dp.update.middleware.register(ThrottlingMiddleware())
+    # Registering middleware for TON Connect processing
+    dp.update.middleware.register(
+        AiogramTonConnectMiddleware(
+            storage=ATCRedisStorage(storage.redis),
+            manifest_url=MANIFEST_URL,
+            exclude_wallets=EXCLUDE_WALLETS,
+            qrcode_provider=QRUrlProvider(),
+        )
+    )
+
+    # Registering TON Connect handlers
+    AiogramTonConnectHandlers().register(dp)
+
+    # Including the router
+    dp.include_router(router)
+
+    # Starting the bot using long polling
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(main())
